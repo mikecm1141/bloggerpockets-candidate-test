4f455480ebe3f9bb378ec8f0eb955c90e2d60cda
Mike <mikecm@gmail.com>
2019-04-13 15:25:40 -0600

    Refactor model to delegate certain model attributes to clean up views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,8 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   scope :published, -> { where(published: true) }
+
+  ## I've delegated the user's name to comment here to remove the need
+  ## for the call in the view.
+  delegate :name, to: :user, prefix: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,8 @@ class Post < ApplicationRecord
   validates_presence_of :body
 
   scope :published, -> { where(published: true) }
+
+  ## As in the Comment model, I have delegated the User's name to the Post
+  ## model to further clean up the view.
+  delegate :name, to: :user, prefix: true
 end
