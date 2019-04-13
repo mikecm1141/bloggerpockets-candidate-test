841c6551380f9b41fd102d7918f8b1458f7dc47c
Mike <mikecm@gmail.com>
2019-04-13 15:46:04 -0600

    Factor out logic into its own method to keep controller action skinny

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  before_action :gather_posts, only: :index
   def index
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def gather_posts
     ## We have a scope defined in the Post model, so we will use that here.
 
     ## I have also used the #includes method here to avoid the N+1 problem,
@@ -12,20 +25,13 @@ class PostsController < ApplicationController
     if params[:sort].present?
       ## Since it is a big security risk here interpolating input from
       ## our incoming params, I will refactor this to have more direct control
-      ## of what method we are using for our AR call.
+      ## of what method we are using for our AR call. I will also implement
+      ## two new scopes to use instead here.
       if params[:sort] == 'asc'
-        @posts = @posts.order(created_at: :asc)
+        @posts = @posts.sort_by_oldest
       elsif params[:sort] == 'desc'
-        @posts = @posts.order(created_at: :desc)
+        @posts = @posts.sort_by_newest
       end
     end
-
-    respond_to do |format|
-      format.html
-    end
-  end
-
-  def show
-    @post = Post.find(params[:id])
   end
 end
