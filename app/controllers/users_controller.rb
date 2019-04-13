65985301f851e45c31a54aeedafd4c748b50a208
Mike <mikecm@gmail.com>
2019-04-13 15:36:31 -0600

    Refactor JSON output to reflect data given in the view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,20 @@ class UsersController < ApplicationController
     ## We can remove the 2nd instance variable since we have the relation
     ## specified for Posts in our User model.
     @user = User.find(params[:id])
-    @posts = @user.posts
 
     respond_to do |format|
       format.html
-      format.json { render json: @user }
+      ## Before this simply outputted the user's entire information in JSON
+      ## format. I think to better reflect what this action does in its HTML
+      ## view, I have refactored it just to include the user's id and name,
+      ## excluding information such as the password_digest field. It also
+      ## includes the user's posts to also reflect what the HTML view gives.
+
+      ## I would normally use a serializer for this purpose to give even
+      ## greater control, but did not feel the need for that in this challenge.
+      format.json do
+        render json: { id: @user.id, name: @user.name, posts: @user.posts }
+      end
     end
   end
 end
