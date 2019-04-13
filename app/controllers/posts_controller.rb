e48b3843e88f054186b3a92da5b2e8eb95d50d5a
Mike <mikecm@gmail.com>
2019-04-13 14:40:18 -0600

    Add #includes method to AR call to gather all published posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,11 @@
 class PostsController < ApplicationController
   def index
     ## We have a scope defined in the Post model, so we will use that here.
-    @posts = Post.published
+
+    ## I have also used the #includes method here to avoid the N+1 problem,
+    ## taking the number of queries for this action to just 2 instead of
+    ## the hundreds it was before.
+    @posts = Post.published.includes(:user)
 
     if params[:sort].present?
       @posts = @posts.order("created_at #{params[:sort]}")
