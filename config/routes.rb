d81bc350d2df5ec5fb24b801bc90a443040eed90
Mike <mikecm@gmail.com>
2019-04-13 14:53:02 -0600

    Refactor resource routes to be more explicit with actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :posts do
+  ## Limit routes to only the ones we are using.
+  resources :posts, only: %i[index show] do
     get :comments, on: :member
   end
 
-  resources :users
+  ## Limit routes to only the ones we are using.
+  resources :users, only: :show
 
   root to: "posts#index"
 end
