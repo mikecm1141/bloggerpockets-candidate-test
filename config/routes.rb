b7c1ec43aadf1974435f09f6bfd1defa1eef9c8d
Mike <mikecm@gmail.com>
2019-04-13 15:11:32 -0600

    Refactor nested route to use a Rails index action for comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@
 Rails.application.routes.draw do
   ## Limit routes to only the ones we are using.
   resources :posts, only: %i[index show] do
-    get :comments, on: :member
+    ## Reconfigure the nested route to use a more conformed controller
+    ## action method, so that we use the Comments controller to gather
+    ## comments and not the Post controller via the custom action #comments.
+    resources :comments, only: :index
   end
 
   ## Limit routes to only the ones we are using.
